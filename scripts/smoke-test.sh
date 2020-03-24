2f4aad2fe9763c520406219f37d1d7c935170519
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-03-24 07:09:40 -0400

    chore: add script to make adding smoke tests easier for other ecosystems

---
+++ scripts/smoke-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+target=$1
+yarn link
+
+cd smoke-tests/$1
+
+yarn
+yarn link eslint-plugin-decorator-position
+yarn eslint \
+  --no-ignore \
+  --no-eslintrc \
+  --config ../../lib/config/$1.js \
+  .
