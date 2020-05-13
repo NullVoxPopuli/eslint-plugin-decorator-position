e82992c268ca817d78f81e74271c0618cfe281ef
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-05-13 07:54:36 -0400

    fix(tests): ember smoke test by upgrading eslint
    Had to adjust where the config is specified from in the bash script

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -2,19 +2,25 @@
 set -e
 
 target=$1
+
+name="eslint-plugin-decorator-position"
+
 yarn
 yarn link
 
 cd smoke-tests/$1
 
-config_path="../../lib/config/$1.js"
+config_path="node_modules/$name/lib/config/$1.js"
 
 if [[ $target == *"rules"* ]]; then
   config_path=".eslintrc.js"
 fi
 
 yarn
-yarn link eslint-plugin-decorator-position
+yarn link $name
+
+# ls -la node_modules/$name
+
 yarn eslint \
   --no-ignore \
   --no-eslintrc \
