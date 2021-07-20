672194e772fab3025ebfbd9ad1aa2c3efde2244c
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-07-20 09:36:01 -0400

    chore(internal): clean up test example for 313

--- smoke-tests/issue-reproductions/313/app.ts
+++ smoke-tests/issue-reproductions/313/app.ts
@@ -2,14 +2,14 @@ function State() {
   return (...decoratorArgs: any[]) => {
     /* This is totally a decorator */
     console.log(decoratorArgs);
-  }
+  };
 }
 
 export class MyComponent {
   checkedClass = () => {};
   disabledClass = () => {};
 
-  @State() classes = ["toggle", this.checkedClass(), this.disabledClass()]
+  @State() classes = ['toggle', this.checkedClass(), this.disabledClass()]
     .filter(Boolean)
-    .join(" ");
+    .join(' ');
 }
