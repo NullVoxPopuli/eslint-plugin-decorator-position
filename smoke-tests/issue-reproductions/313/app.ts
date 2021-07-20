5bfcb0eb3cff5d75a16619e4c191443678baaa75
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-07-19 20:34:03 -0400

    chore(internal): add reproduction for issue #313

---
+++ smoke-tests/issue-reproductions/313/app.ts
@@ -0,0 +1,15 @@
+function State() {
+  return (...decoratorArgs: any[]) => {
+    /* This is totally a decorator */
+    console.log(decoratorArgs);
+  }
+}
+
+export class MyComponent {
+  checkedClass = () => {};
+  disabledClass = () => {};
+
+  @State() classes = ["toggle", this.checkedClass(), this.disabledClass()]
+    .filter(Boolean)
+    .join(" ");
+}
