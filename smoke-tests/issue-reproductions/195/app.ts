7d9c57345df631c88f8c30b6a8191983a649064a
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-09-18 20:18:06 +0100

    aye

--- smoke-tests/issue-reproductions/195/app.ts
+++ smoke-tests/issue-reproductions/195/app.ts
@@ -1,6 +1,8 @@
 type Service = unknown;
 
-function service(name) {
+// this isn't part of the test for issue 195
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function service(name: string) {
   return function decorator(target: unknown, key: string, descriptor?: PropertyDescriptor): void {
     console.log(target, key, descriptor);
   };
